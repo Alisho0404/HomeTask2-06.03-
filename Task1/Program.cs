fd1673d9dcfb0646f507b8a7180d3de94a2284be
Alisho0404 <alishoanvarsho@mail.ru>
2024-03-07 16:02:38 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime date=new DateTime(1999,10,20,12,54,55); 
+System.Console.WriteLine("Year: "+date.Year); 
+System.Console.WriteLine("Month: "+date.Month);
+System.Console.WriteLine("Day: "+date.Day);
+System.Console.WriteLine("Hour: "+date.Hour);
+System.Console.WriteLine("Minute: "+date.Minute);
+System.Console.WriteLine("Second: "+date.Second);
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int count=0;  
+System.Console.WriteLine("Enter words"); 
+System.Console.WriteLine("---------------------");
+ while (true)
+{
+    string word=Console.ReadLine(); 
+    if (word=="end")
+    { 
+       break;        
+    }
+    count++;
+    
+} 
+System.Console.WriteLine("Count of word except <end> is: "+count);
